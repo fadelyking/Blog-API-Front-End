9c19f14176cd1b07d75aacb4a298d3daa8571081
fadelyking <fadely195@gmail.com>
2024-01-16 06:41:19 +0300

    fetching from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import NavBar from "./Navigation";
+
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [blogs, setBlogs] = useState([{}]);
 
+	useEffect(() => {
+		function getPosts() {
+			fetch("http://localhost:3000/", { mode: "cors" })
+				.then((response) => {
+					if (response.status >= 400) {
+						throw new Error("server error");
+					}
+					return response.json();
+				})
+				.then((json) => setBlogs(json))
+				.catch((error) => console.log(error));
+		}
+
+		getPosts();
+	}, [])
+	
 	return (
 		<>
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1 className="">Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+			<NavBar></NavBar>
+			
 		</>
 	);
 }
