feb28c6e7d52a5aa4b856ffb0a3d0eb2a69af488
fadelyking <fadely195@gmail.com>
2024-01-16 07:59:19 +0300

    display each blog in grind

---
+++ src/Homepage.jsx
@@ -0,0 +1,20 @@
+import { v4 as uuidv4 } from "uuid";
+export default function Home({ blogs }) {
+  console.log(blogs);
+
+  return (
+    <div className="flex items-center flex-col">
+      <div className="text-xl p-5 font-bold mt-4">Trending</div>
+      <div className="grid grid-cols-3 gap-40 mt-10">
+        {blogs.posts.map((post, i) => (
+          <div key={post._id} className=" col-auto">
+            <li className=" list-none font-bold mt-2">{post.title}</li>
+            <li className=" list-none text-xs">
+              {post.user.first_name + " " + post.user.last_name}
+            </li>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
