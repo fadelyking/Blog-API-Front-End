56bbbaa38abd67a221873753e3eb4ee56b9a9163
fadelyking <fadely195@gmail.com>
2024-01-16 14:28:53 +0300

    sign in form fully styled

--- src/Sign-in.jsx
+++ src/Sign-in.jsx
@@ -4,14 +4,27 @@ export default function SignIn() {
   return (
     <>
       <NavBar></NavBar>
-      <div className="flex items-center flex-col">
-        <div className="text-3xl p-5 font-bold mt-6">Welcome</div>
-        <form className=" flex flex-col" action="" method="post">
-          <label for="Email">Email:</label>
-          <input type="email" name="email" />
-          <label for="Password">Password:</label>
-          <input type="password" name="password" />
-          <input type="submit" value="Submit" />
+      <div className="flex items-center flex-col mt-10 gap-5">
+        <div className="text-3xl p-5 font-bold mt-10">Welcome</div>
+        <form className=" flex flex-col gap-10" action="" method="post">
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            className="drop-shadow-sm border-b-2 border-gray-500 p-2 outline-none"
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            className="drop-shadow-sm border-b-2 border-gray-500 p-2 outline-none"
+          />
+          <button
+            type="submit"
+            className="mt-5 bg-purple-500 p-4 text-white rounded-xl"
+          >
+            Submit
+          </button>
         </form>
       </div>
     </>
